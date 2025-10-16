79fe1f1251c974ce85cf4f357aef0764b58b1cc7
yyliziqiu <yyliziqiu@163.com>
2025-10-16 11:34:55 +0800

    pretty

--- main.go
+++ main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/yyliziqiu/smpp/ex"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: smpp [client]")
-	}
-
-	if os.Args[1] == "client" {
+	if len(os.Args) > 1 && os.Args[1] == "client" {
 		ex.StartClient()
 	} else {
 		ex.StartServer()
