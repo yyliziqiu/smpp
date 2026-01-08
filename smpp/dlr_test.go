1fd644e72ece7e23a37db31148ca92e91d982ccd
yyliziqiu <yyliziqiu@163.com>
2026-01-08 19:56:38 +0800

    pretty

--- smpp/dlr_test.go
+++ smpp/dlr_test.go
@@ -7,10 +7,13 @@ import (
 
 func TestParseDlr(t *testing.T) {
 	strs := []string{
-		"id:1081737143648 sub:000 dlvrd:000 submit date:1767865032314 done date:1767865045669 stat:DELIVRD err:000 text:",
 		"id:1081737143648 sub:000 dlvrd:000 submit date:2605111200 done date:2605111200 stat:DELIVRD err:000 text:",
 		"id:1081737143648 sub:000 dlvrd:000 submit date:260511120000 done date:260511120000 stat:DELIVRD err:000 text:",
+		"id:1081737143648 sub:000 dlvrd:000 submit date:1767865032 done date:1767865045669 stat:DELIVRD err:000 text:",
+		"id:1081737143648 sub:000 dlvrd:000 submit date:1767865032314 done date:1767865045669 stat:DELIVRD err:000 text:",
+		"id:1081737143648 sub:000 dlvrd:000 submit date:176786503 done date:1767865045669 stat:DELIVRD err:000 text:",
 	}
+
 	for _, str := range strs {
 		dlr, err := ParseDlr(str)
 		if err != nil {
