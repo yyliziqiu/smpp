cced450a060594d253de4179554489b433caffd6
yyliziqiu <yyliziqiu@163.com>
2026-06-08 14:17:02 +0800

    pretty

--- smpp/response.go
+++ smpp/response.go
@@ -5,23 +5,37 @@ import (
 )
 
 // Response will be created when received response of transmit pdu or error occurred.
-// The Pdu will be nil if the Error is not nil.
 type Response struct {
+	// indicate this is a failed response, that didn't receive the response pdu
+	Error error
+
+	// this field will be nil if Error is not nil.
+	Pdu pdu.PDU
+
+	// the request of the response
 	Request *Request
-	Pdu     pdu.PDU
-	Error   error
 }
 
 func NewResponse(req *Request, p pdu.PDU, err error) *Response {
-	return &Response{
-		Request: req,
-		Pdu:     p,
-		Error:   err,
-	}
+	return &Response{Error: err, Pdu: p, Request: req}
 }
 
 func (t *Response) TraceData() any {
 	return t.Request.TraceData
+}
+
+func (t *Response) TraceInt() int {
+	if i, ok := t.Request.TraceData.(int); ok {
+		return i
+	}
+	return 0
+}
+
+func (t *Response) TraceString() string {
+	if s, ok := t.Request.TraceData.(string); ok {
+		return s
+	}
+	return ""
 }
 
 func (t *Response) SessionId() string {
@@ -41,18 +55,4 @@ func (t *Response) ErrorString() string {
 		return ""
 	}
 	return t.Error.Error()
-}
-
-func (t *Response) TraceInt() int {
-	if i, ok := t.Request.TraceData.(int); ok {
-		return i
-	}
-	return 0
-}
-
-func (t *Response) TraceString() string {
-	if s, ok := t.Request.TraceData.(string); ok {
-		return s
-	}
-	return ""
 }
